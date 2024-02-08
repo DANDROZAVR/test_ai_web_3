671b5007a52f7ebb25e11a6e2f4adebc69026638
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-02-08 15:44:48 +0100

    Added new functions from input files

--- src/service.py
+++ src/service.py
@@ -3,3 +3,7 @@ console.log('Data loaded: 204 rows');
 logging.debug('Ending process...')
 logging.debug('Starting process...')
 print('Data loaded: 898 rows')
+from web3.types import (
+    Wei,
+)
+
