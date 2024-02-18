99260f8ed77cd4c8e58db0a770e54d20a25f59c8
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-02-18 15:44:48 +0100

    Incorporated code snippets from input files

---
+++ src/runner.py
@@ -0,0 +1,10 @@
+# Auto-generated file
+class InvalidTransaction(Web3Exception):
+    """
+    Raised when a transaction includes an invalid combination of arguments.
+    """
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
