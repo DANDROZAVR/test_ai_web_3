85fc14969f1e15e30d4ef37bfd374e94e0345750
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-02-19 15:44:48 +0100

    Inserted code blocks from input source

--- src/module.py
+++ src/module.py
@@ -1,2 +1,24 @@
 # Auto-generated file
 print('Data loaded: 970 rows')
+class StaleBlockchain(Web3Exception):
+    """
+    Raised by the stalecheck_middleware when the latest block is too old.
+    """
+
+    def __init__(self, block: BlockData, allowable_delay: int) -> None:
+        last_block_date = datetime.datetime.fromtimestamp(block["timestamp"]).strftime(
+            "%c"
+        )
+        message = (
+            f"The latest block, #{block['number']}, is "
+            f"{time.time() - block['timestamp']} seconds old, but is only "
+            f"allowed to be {allowable_delay} s old. "
+            f"The date of the most recent block is {last_block_date}. Continue "
+            "syncing and try again..."
+        )
+        super().__init__(message, block, allowable_delay)
+
+    def __str__(self) -> str:
+        return self.args[0]
+
+
