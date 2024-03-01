06d28fff21d68de77e939c6ba95b74189c7927db
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-03-01 15:44:48 +0100

    Integrated reusable code blocks

--- src/builder.py
+++ src/builder.py
@@ -1,1 +1,7 @@
- 
+ from web3.middleware import (
+    AttributeDictMiddleware,
+    BufferedGasEstimateMiddleware,
+    ENSNameToAddressMiddleware,
+    GasPriceStrategyMiddleware,
+    ValidationMiddleware,
+)
