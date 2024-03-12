319f36468ead14d435aab33b4b2cf74823cc5194
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-03-12 15:44:48 +0100

    Incorporated code snippets from input files

--- src/runner.py
+++ src/runner.py
@@ -10,3 +10,18 @@ class InvalidTransaction(Web3Exception):
 
 logging.debug('User logged in: user72')
 console.log('Operation completed successfully');
+def _set_mungers(
+    mungers: Optional[Sequence[Munger]], is_property: bool
+) -> Sequence[Any]:
+    if is_property and mungers:
+        raise Web3ValidationError("Mungers cannot be used with a property.")
+
+    return (
+        mungers
+        if mungers
+        else [default_munger]
+        if is_property
+        else [default_root_munger]
+    )
+
+
