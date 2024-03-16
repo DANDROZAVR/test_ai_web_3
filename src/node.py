8c0609b8780c25332a51a616081501cd1df23c80
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-03-16 15:44:48 +0100

    Integrated reusable code blocks

--- src/node.py
+++ src/node.py
@@ -4,4 +4,30 @@ class PersistentConnectionError(Web3Exception):
     """
 
 
-  
+  class Web3Exception(Exception):
+    """
+    Exception mixin inherited by all exceptions of web3.py
+
+    This allows::
+
+        try:
+            some_call()
+        except Web3Exception:
+            # deal with web3 exception
+        except:
+            # deal with other exceptions
+    """
+
+    user_message: Optional[str] = None
+
+    def __init__(
+        self,
+        *args: Any,
+        user_message: Optional[str] = None,
+    ):
+        super().__init__(*args)
+
+        # Assign properties of Web3Exception
+        self.user_message = user_message
+
+
