33d504acdf6789fc68b274dc776eb38d8b35a09e
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-04-07 15:44:48 +0200

    Reorganized functions for clarity

--- src/service.py
+++ src/service.py
@@ -1,10 +1,10 @@
+from eth_utils.curried import (
+    to_tuple,
+)
 from web3.types import (
     Wei,
 )
 
-from eth_utils.curried import (
-    to_tuple,
-)
 from web3.tracing import (
     Tracing,
 )
