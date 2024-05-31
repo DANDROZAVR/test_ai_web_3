d3a050f2bceef0d416bd513290d04086460e2e3b
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-05-31 15:44:48 +0200

    Restructured code for better readability

--- src/app.py
+++ src/app.py
@@ -1,14 +1,12 @@
- console.log('User logged in: user17');
-logger.info('Operation completed successfully')
-class ContractPanicError(ContractLogicError):
-    """
-    Raised when a contract reverts with Panic, as of Solidity 0.8.0
-    """
-
-
 class MethodNotSupported(Web3Exception):
     """
     Raised when a method is not supported by the provider.
+    """
+
+
+class ContractPanicError(ContractLogicError):
+    """
+    Raised when a contract reverts with Panic, as of Solidity 0.8.0
     """
 
 
