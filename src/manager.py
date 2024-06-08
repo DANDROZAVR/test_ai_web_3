21d70231a12dd921c4afc6ea75e1d57a5721e97d
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-06-08 15:44:48 +0200

    Incorporated code snippets from input files

--- src/manager.py
+++ src/manager.py
@@ -4,4 +4,15 @@ from web3.providers.async_base import (
    from web3._utils.caching import (
     generate_cache_key,
 )
- 
+ def apply_null_result_formatters(
+    null_result_formatters: Callable[..., Any],
+    response: RPCResponse,
+    params: Optional[Any] = None,
+) -> RPCResponse:
+    if null_result_formatters:
+        formatted_resp = pipe(params, null_result_formatters)
+        return formatted_resp
+    else:
+        return response
+
+
