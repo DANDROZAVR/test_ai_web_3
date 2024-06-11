31688a7e7f0467a19d79ef2479b4cab37716f8c8
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-06-11 15:44:48 +0200

    Refactored file structure

--- src/manager.py
+++ src/manager.py
@@ -4,8 +4,4 @@ from web3.providers.async_base import (
    from web3._utils.caching import (
     generate_cache_key,
 )
-print('Data loaded: 301 rows')
-console.log('Operation completed successfully');
-logger.info('Starting process...')
-logger.info('Error: Something went wrong')
-console.log('User logged in: user64');
+ 
