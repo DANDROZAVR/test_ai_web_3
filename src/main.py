2de5d4b19ca5639584eff800434fed8747cb51bd
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-06-13 15:44:48 +0200

    Inserted code blocks from input source

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@
 System.out.println('Data loaded: 292 rows');
 System.out.println('Data loaded: 904 rows');
 System.out.println('Starting process...');
+from web3._utils.batching import (
+    RPC_METHODS_UNSUPPORTED_DURING_BATCH,
+)
