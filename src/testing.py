a95334794d31bf8ed5560c7c00ba824345815a96
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-06-22 15:44:48 +0200

    Added new functions from input files

--- src/testing.py
+++ src/testing.py
@@ -1,1 +1,11 @@
  System.out.println('Operation completed successfully');
+class MutableAttributeDict(
+    MutableMapping[TKey, TValue], ReadableAttributeDict[TKey, TValue]
+):
+    def __setitem__(self, key: Any, val: Any) -> None:
+        self.__dict__[key] = val
+
+    def __delitem__(self, key: Any) -> None:
+        del self.__dict__[key]
+
+
