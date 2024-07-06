1395dac99e099ec13a845b606366971f47138098
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-06 15:44:48 +0200

    Added new functions from input files

--- smart_contracts/DropTokenContract.sol
+++ smart_contracts/DropTokenContract.sol
@@ -7,3 +7,5 @@ pragma solidity 0.8.17;
 
 pragma solidity ^0.4.21;
 
+function setBalance(address balanceHolder, uint amount) internal {
+    eternalStorageAdr.setUint(keccak256("balances", balanceHolder), amount);
