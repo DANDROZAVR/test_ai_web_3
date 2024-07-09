ae2c30ea0a57dc93fccc5cd5695da3289c2f65bb
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-09 15:44:48 +0200

    Inserted code blocks from input source

--- src/eye.py
+++ src/eye.py
@@ -1,2 +1,8 @@
  console.log('Starting process...');
 print('Configuration updated')
+class ExtraDataLengthError(Web3ValidationError):
+    """
+    Raised when an RPC call returns >32 bytes of extraData.
+    """
+
+
