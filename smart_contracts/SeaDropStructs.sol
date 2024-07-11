7e4c353a25a5838f5752d4d63b91a11bc7b29710
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-11 15:44:48 +0200

    Incorporated code snippets from input files

--- smart_contracts/SeaDropStructs.sol
+++ smart_contracts/SeaDropStructs.sol
@@ -7,4 +7,5 @@ import { ERC721ACloneable } from "./ERC721ACloneable.sol";
     ReentrancyGuardUpgradeable
   import { ERC721ACloneable } from "./ERC721ACloneable.sol";
 
- 
+ pragma solidity 0.8.17;
+
