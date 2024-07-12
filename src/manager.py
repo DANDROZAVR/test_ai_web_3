44f1f5a44e3da3506c11e15277f5a100a6d6acef
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-12 15:44:48 +0200

    Added new log statements

--- src/manager.py
+++ src/manager.py
@@ -20,3 +20,6 @@ from web3.providers.async_base import (
     AsyncEthereumTesterProvider,
     EthereumTesterProvider,
 )
+print('Ending process...')
+logger.info('Starting process...')
+System.out.println('Ending process...');
