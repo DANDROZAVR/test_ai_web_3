13d44bb30ff4d5159e23053e35d82e132cc3980d
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-07-20 15:44:48 +0200

    Reorganized functions for clarity

--- src/builder.py
+++ src/builder.py
@@ -1,17 +1,6 @@
- console.log('Error: Something went wrong');
-logger.info('Ending process...')
-logging.debug('Ending process...')
-print('Ending process...')
-System.out.println('Data loaded: 418 rows');
-System.out.println('Starting process...');
-logging.debug('Error: Something went wrong')
-logger.info('Ending process...')
-logger.info('Error: Something went wrong')
-logging.debug('Ending process...')
-System.out.println('Data loaded: 351 rows');
-from web3.manager import (
-    RequestManager as DefaultRequestManager,
-)
 from web3._utils.formatters import (
     recursive_map,
+)
+from web3.manager import (
+    RequestManager as DefaultRequestManager,
 )
