5494f0b16a89ac26966542635f39a6fa00d09aa2
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-14 15:44:48 +0200

    Restructured code for better readability

--- src/runner.py
+++ src/runner.py
@@ -1,3 +1,55 @@
+class ReadableAttributeDict(Mapping[TKey, TValue]):
+    """
+    The read attributes for the AttributeDict types
+    """
+
+    def __init__(
+        self, dictionary: Dict[TKey, TValue], *args: Any, **kwargs: Any
+    ) -> None:
+        # type ignored on 46/50 b/c dict() expects str index type not TKey
+        self.__dict__ = dict(dictionary)  # type: ignore
+        self.__dict__.update(dict(*args, **kwargs))
+
+    def __getitem__(self, key: TKey) -> TValue:
+        return self.__dict__[key]  # type: ignore
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self.__dict__)
+
+    def __len__(self) -> int:
+        return len(self.__dict__)
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__ + f"({self.__dict__!r})"
+
+    def _repr_pretty_(self, builder: Any, cycle: bool) -> None:
+        """
+        Custom pretty output for the IPython console
+        https://ipython.readthedocs.io/en/stable/api/generated/IPython.lib.pretty.html#extending  # noqa: E501
+        """
+        builder.text(self.__class__.__name__ + "(")
+        if cycle:
+            builder.text("<cycle>")
+        else:
+            builder.pretty(self.__dict__)
+        builder.text(")")
+
+    @classmethod
+    def _apply_if_mapping(cls: Type[T], value: TValue) -> Union[T, TValue]:
+        if isinstance(value, Mapping):
+            # error: Too many arguments for "object"
+            return cls(value)  # type: ignore
+        else:
+            return value
+
+    @classmethod
+    def recursive(cls, value: TValue) -> "ReadableAttributeDict[TKey, TValue]":
+        return cast(
+            "ReadableAttributeDict[TKey, TValue]",
+            recursive_map(cls._apply_if_mapping, value),
+        )
+
+
 class InvalidTransaction(Web3Exception):
     """
     Raised when a transaction includes an invalid combination of arguments.
@@ -80,58 +132,3 @@ def apply_error_formatters(
         return response
 
 
-class ReadableAttributeDict(Mapping[TKey, TValue]):
-    """
-    The read attributes for the AttributeDict types
-    """
-
-    def __init__(
-        self, dictionary: Dict[TKey, TValue], *args: Any, **kwargs: Any
-    ) -> None:
-        # type ignored on 46/50 b/c dict() expects str index type not TKey
-        self.__dict__ = dict(dictionary)  # type: ignore
-        self.__dict__.update(dict(*args, **kwargs))
-
-    def __getitem__(self, key: TKey) -> TValue:
-        return self.__dict__[key]  # type: ignore
-
-    def __iter__(self) -> Iterator[Any]:
-        return iter(self.__dict__)
-
-    def __len__(self) -> int:
-        return len(self.__dict__)
-
-    def __repr__(self) -> str:
-        return self.__class__.__name__ + f"({self.__dict__!r})"
-
-    def _repr_pretty_(self, builder: Any, cycle: bool) -> None:
-        """
-        Custom pretty output for the IPython console
-        https://ipython.readthedocs.io/en/stable/api/generated/IPython.lib.pretty.html#extending  # noqa: E501
-        """
-        builder.text(self.__class__.__name__ + "(")
-        if cycle:
-            builder.text("<cycle>")
-        else:
-            builder.pretty(self.__dict__)
-        builder.text(")")
-
-    @classmethod
-    def _apply_if_mapping(cls: Type[T], value: TValue) -> Union[T, TValue]:
-        if isinstance(value, Mapping):
-            # error: Too many arguments for "object"
-            return cls(value)  # type: ignore
-        else:
-            return value
-
-    @classmethod
-    def recursive(cls, value: TValue) -> "ReadableAttributeDict[TKey, TValue]":
-        return cast(
-            "ReadableAttributeDict[TKey, TValue]",
-            recursive_map(cls._apply_if_mapping, value),
-        )
-
-
-System.out.println('Ending process...');
-logger.info('Error: Something went wrong')
-console.log('Starting process...');
