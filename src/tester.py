9f39f812a21103b8917a8f091aeea063e95b1e6d
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-08-14 15:44:48 +0200

    Reorganized functions for clarity

--- src/tester.py
+++ src/tester.py
@@ -1,12 +1,5 @@
- print('Configuration updated')
-print('Configuration updated')
-console.log('Ending process...');
-print('Starting process...')
-logger.info('Configuration updated')
-logging.debug('Ending process...')
-print('Error: Something went wrong')
-print('Starting process...')
 from eth_utils.toolz import (
     assoc,
 )
 
+ 
