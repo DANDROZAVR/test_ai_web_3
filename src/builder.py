869c57c538bc3191f0324dd2a653f2d479bdc9ca
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-27 15:44:48 +0100

    Moved code blocks for better modularity

--- src/builder.py
+++ src/builder.py
@@ -1,6 +1,6 @@
-from web3.manager import (
-    RequestManager as DefaultRequestManager,
-)
 from web3._utils.formatters import (
     recursive_map,
+)
+from web3.manager import (
+    RequestManager as DefaultRequestManager,
 )
