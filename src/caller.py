8ad63e9508269bce8eda22a06d8d4ec6da216412
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-10-31 15:44:48 +0100

    Added new functions from input files

--- src/caller.py
+++ src/caller.py
@@ -13,3 +13,19 @@ class PersistentConnectionError(Web3Exception):
     """
 
 
+class TaskNotRunning(Web3Exception):
+    """
+    Used to signal between asyncio contexts that a task that is being awaited
+    is not currently running.
+    """
+
+    def __init__(
+        self, task: "asyncio.Task[Any]", message: Optional[str] = None
+    ) -> None:
+        self.task = task
+        if message is None:
+            message = f"Task {task} is not running."
+        self.message = message
+        super().__init__(message)
+
+
