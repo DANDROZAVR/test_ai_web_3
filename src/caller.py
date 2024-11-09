5120f497230ebb12495bb1074bdd81fac43ba0b5
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-09 15:44:48 +0100

    Integrated reusable code blocks

--- src/caller.py
+++ src/caller.py
@@ -29,3 +29,13 @@ class TaskNotRunning(Web3Exception):
         super().__init__(message)
 
 
+def apply_result_formatters(
+    result_formatters: Callable[..., Any], result: RPCResponse
+) -> RPCResponse:
+    if result_formatters:
+        formatted_result = pipe(result, result_formatters)
+        return formatted_result
+    else:
+        return result
+
+
