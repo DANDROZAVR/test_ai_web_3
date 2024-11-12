e1c6ec47515504bded7ef4445962b6deca7d9476
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-12 15:44:48 +0100

    Restructured code for better readability

--- smart_contracts/SeaDropStructs.sol
+++ smart_contracts/SeaDropStructs.sol
@@ -1,10 +1,10 @@
+import { IERC2981 } from "openzeppelin-contracts/interfaces/IERC2981.sol";
+
 import {
     AllowListData,
     PublicDrop,
     TokenGatedDropStage,
     SignedMintValidationParams
-import { IERC2981 } from "openzeppelin-contracts/interfaces/IERC2981.sol";
-
 import { ERC721ACloneable } from "./ERC721ACloneable.sol";
 
      contract ERC721SeaDropCloneable is
