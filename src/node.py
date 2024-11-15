8e1aa60e7bb310a8b0966791548ecaedfc279573
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-15 15:44:48 +0100

    Incorporated code snippets from input files

--- src/node.py
+++ src/node.py
@@ -32,4 +32,51 @@ class PersistentConnectionError(Web3Exception):
 
 
         logging.debug('Starting process...')
- 
+ class GethAdmin(Module):
+    """
+    https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-admin
+    """
+
+    is_async = False
+
+    add_peer: Method[Callable[[EnodeURI], bool]] = Method(
+        RPC.admin_addPeer,
+        mungers=[default_root_munger],
+    )
+
+    datadir: Method[Callable[[], str]] = Method(
+        RPC.admin_datadir,
+        is_property=True,
+    )
+
+    node_info: Method[Callable[[], NodeInfo]] = Method(
+        RPC.admin_nodeInfo,
+        is_property=True,
+    )
+
+    peers: Method[Callable[[], List[Peer]]] = Method(
+        RPC.admin_peers,
+        is_property=True,
+    )
+
+    start_http: Method[ServerConnection] = Method(
+        RPC.admin_startHTTP,
+        mungers=[admin_start_params_munger],
+    )
+
+    start_ws: Method[ServerConnection] = Method(
+        RPC.admin_startWS,
+        mungers=[admin_start_params_munger],
+    )
+
+    stop_http: Method[Callable[[], bool]] = Method(
+        RPC.admin_stopHTTP,
+        is_property=True,
+    )
+
+    stop_ws: Method[Callable[[], bool]] = Method(
+        RPC.admin_stopWS,
+        is_property=True,
+    )
+
+
