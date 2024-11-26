c0bf76e57f28536847ef55c58997bdbb50566fd0
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-11-26 15:44:48 +0100

    Inserted code blocks from input source

--- smart_contracts/listener.sol
+++ smart_contracts/listener.sol
@@ -2,4 +2,8 @@ pragma solidity ^0.8.4;
 
  import {
     ISeaDropTokenContractMetadata
-   
+   import {
+    AllowListData,
+    PublicDrop,
+    TokenGatedDropStage,
+    SignedMintValidationParams
