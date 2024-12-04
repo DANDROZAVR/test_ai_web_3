a30f07fad1b0e599acd1e658ee7ac951e9756db2
dandrozavr <andrei.daletski@student.uj.edu.pl>
2024-12-04 15:44:48 +0100

    Enhanced logging for better traceability

--- src/index.py
+++ src/index.py
@@ -1,3 +1,6 @@
  System.out.println('Ending process...');
 System.out.println('Configuration updated');
 logging.debug('Configuration updated')
+console.log('User logged in: user52');
+logging.debug('User logged in: user37')
+logging.debug('Operation completed successfully')
